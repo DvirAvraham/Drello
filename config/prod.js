02d0c75981ea8d20bd987d423f88ffb29f7a6b1d
Eden Avidan <edenaavidan@gmail.com>
2022-03-31 14:54:07 +0300

    heroku upload

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    dbURL: process.env.DB_KEY
+    dbURL:
+        'mongodb+srv://mongoSprint:Eb36Ywb*Q2TY&dw^qQ4f@cluster0.yyebs.mongodb.net/board_db?retryWrites=true&w=majority'
 }
